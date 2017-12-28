50c86cbb0670bc257bb99836ae68f54baffb7bed
Aaron Berry <aaron.h.berry@att.net>
2017-12-28 00:35:43 -0500

    changed the name on rb files

--- cookbooks/iis/recipes/mod_common.rb
+++ cookbooks/iis/recipes/mod_common.rb
@@ -1,11 +1,10 @@
 #
-# Cookbook:: iis
-# Recipe:: Common_Http_Features
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+## Cookbook:: iis
+## Recipe:: Common_Http_Features
+##
+## Copyright:: 2017, The Authors, All Rights Reserved.
 powershell_script 'Install Common_Http_Features' do
 	code 'Add-WindowsFeature -Name Web-Http-Redirect'
 	guard_interpreter :powershell_script
 	not_if "(Get-WindowsFeature -Name Web-Http-Redirect).Installed"
 end
-
---
+++ cookbooks/iis/recipes/mod_iis_settings.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: iis
+# Recipe:: configure_iis_settings
+#
+# Copyright:: 2017, Aaron Berry, All Rights Reserved.
+
+
+
+
